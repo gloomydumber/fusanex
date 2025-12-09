85b4120cad5ddbd407d055b9527406e7146efb51
gloomydumber <chromacrow@naver.com>
2025-12-10 02:50:04 +0900

    fix: remove base-asset.ts

--- domain/base-asset.ts
+++
@@ -1,7 +0,0 @@
-export type BaseAsset = 'BTC' | 'ETH' | 'XRP' | 'BNB' | 'SOL';
-
-export const BASE_ASSETS: BaseAsset[] = ['BTC', 'ETH', 'XRP', 'BNB', 'SOL'];
-
-export function isBaseAsset(value: string): value is BaseAsset {
-  return (BASE_ASSETS as string[]).includes(value);
-}
