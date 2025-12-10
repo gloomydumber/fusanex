749dffc43ba33306bb6f066d307ed35fab7657e1
gloomydumber <chromacrow@naver.com>
2025-12-10 22:52:10 +0900

    fix: remove domain config

--- domain/config.ts
+++
@@ -1,7 +0,0 @@
-import type { CurrencyCode } from './currency';
-
-export interface PusanexDomainConfig {
-  // We can loosely type this as CurrencyCode now, or keep a stricter CryptoCurrency type if desired.
-  // Generally, the base asset for cross-rates is a high-liquidity crypto.
-  baseAsset: CurrencyCode;
-}
